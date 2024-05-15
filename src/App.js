26148891217a4dec77d8b4f8d78d4b7cdd2e4075
myeongeun <c_me95@mentorit.co.kr>
2024-05-15 22:43:13 +0900

    프로젝트 1. Navbar, layout 구성 및 페이지 연결

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes } from "react-router-dom";
 import AppLayout from "./layout/AppLayout";
+import Homepage from "./pages/Homepage/Homepage";
+import MoviePage from "./pages/Movies/MoviePage";
+import MovieDetailPage from "./pages/MovieDetail/MovieDetailPage";
+import Login from "./pages/Login/Login";
+import NotFoundPage from "./pages/NotFoundpage/NotFoundPage";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={ <AppLayout /> } />
+          <Route path="/" element={ <AppLayout/> }>
+              <Route index element={<Homepage/>} />
+              <Route path="/login" element={<Login/>} />
+
+              {/*<Route path="/movies" element={<MoviePage/>} />*/}
+              {/*<Route path="/movies/:id" element={<MovieDetailPage/>} />*/}
+              <Route path="movies">
+                  <Route index element={<MoviePage/>} />
+                  <Route path=":id" element={<MovieDetailPage/>} />
+              </Route>
+
+              <Route path="*" element={<NotFoundPage/>} />
+          </Route>
       </Routes>
     </div>
   );
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -10,21 +10,26 @@ const Login = () => {
     };
 
     return (
-        <Container>
-            <h1>영화, 시리즈 등을 무제한으로</h1>
-            <h2>어디서나 자유롭게 시청하세요. 해지는 언제든 가능합니다.</h2>
-            <h3>시청할 준비가 되셨나요? 멤버십을 등록하거나 재시작하려면 이메일 주소를 입력하세요.</h3>
+        <div className="login-page">
+            <Container className="login-page-box">
+                <h1 className="text-white">영화, 시리즈 등을 무제한으로</h1>
+                <p className="text-white">어디서나 자유롭게 시청하세요. 해지는 언제든 가능합니다.</p>
 
-            <Form onSubmit={ btnLoginClick }>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Control type="email" placeholder="이메일 주소"/>
-                </Form.Group>
-
-                <Button className="btn-danger" type="submit">
-                    시작하기 >
-                </Button>
-            </Form>
-        </Container>
+                <div>
+                    <h3 className="text-white">시청할 준비가 되셨나요? 멤버십을 등록하거나 재시작하려면 이메일 주소를 입력하세요.</h3>
+                    <div>
+                        <Form onSubmit={btnLoginClick}>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Control type="email" placeholder="이메일 주소"/>
+                                <Button className="btn-danger" type="submit">
+                                    시작하기 >
+                                </Button>
+                            </Form.Group>
+                        </Form>
+                    </div>
+                </div>
+            </Container>
+        </div>
     );
 };
 
